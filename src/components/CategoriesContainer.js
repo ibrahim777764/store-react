98ee4d3affdf0d1ab0412f30685bd88aedc92629
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-07-08 17:59:19 +0500

    created file

---
+++ src/components/CategoriesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CategoriesContainer() {
+  return (
+    <div className="container">
+      <h2>Categories</h2>
+      <p>Under Contruction</p>
+    </div>
+  );
+}
+
+export default CategoriesContainer;
